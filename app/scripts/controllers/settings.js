7f600ce3030c45ee0f16935994558da370c00e28
Chris Gunawardena <chris.gunawardena@me.com>
2014-04-30 01:25:40 +1000

    RC1

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -22,11 +22,11 @@ angular.module('submitRequestApp')
     $scope.ip_address = '127.0.0.1';
     $scope.add_ip = function(ip) {
 			if( $scope.currentUser.ip_addresses.indexOf(ip) === -1 )
-			{	console.log( 'User.update({ new_ip: ip })' );
+			{	//console.log( 'User.update({ new_ip: ip })' );
         User.update( { new_ip: ip },
           function(user) {
-            console.log( "RETURNED USER AFER UPDATE" );
-            console.log( user );
+            //console.log( "RETURNED USER AFER UPDATE" );
+            //console.log( user );
             $rootScope.currentUser = user;
           }
         );
@@ -34,11 +34,11 @@ angular.module('submitRequestApp')
     };
 
     $scope.remove_ip = function(ip) {
-      console.log( 'User.update({ remove_ip: ip })' );
+      //console.log( 'User.update({ remove_ip: ip })' );
       User.update( { remove_ip: ip },
         function(user) {
-          console.log( "RETURNED USER AFER UPDATE" );
-          console.log( user );
+          //console.log( "RETURNED USER AFER UPDATE" );
+          //console.log( user );
           $rootScope.currentUser = user;
         }
       );
